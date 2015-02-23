fec483f675774a717396b858d389c13a7993716d
Quazaka <mcmi13@student.aau.dk>
2015-02-23 12:15:50 +0100

    small fix

--- SyntaxSuggestions/Java.c
+++ SyntaxSuggestions/Java.c
@@ -1,20 +1,20 @@
 int main()
 {
-    int num;
-
     /*Array*/
     array A[5] = {1,2,3,4,5};
+    /* empty 2x2 matrix */
+    mx V0[2,2];
 
     /*1D matrix*/
-    mx V1[1,2,3,4,5];
+    mx V1{1,2,3,4,5};
 
     /*2D matrix*/
-    mx V2[1,1,1,1,1|
+    mx V2{1,1,1,1,1|
           2,2,2,2,2|
-          3,3,3,3,3];
+          3,3,3,3,3};
 
     /*3D matrix http://se.mathworks.com/help/matlab/math/ch_data_struct4.gif */
-    mx V3[(1,1,1,1,1|
+    mx V3{(1,1,1,1,1|
            2,2,2,2,2|
            3,3,3,3,3)
                 (1,1,1,1,1|
@@ -22,7 +22,7 @@ int main()
                 3,3,3,3,3)
                     (1,1,1,1,1|
                     2,2,2,2,2|
-                    3,3,3,3,3)];
+                    3,3,3,3,3)};
     /* compute and print */	   
     V4 = V2 * V1; /* "*", "/", "+", "-" */
     print("matrix:" + V3);
@@ -32,10 +32,11 @@ int main()
     for(1:10:+1)
     {
         V2 = V2 * 2;
-        print(V2)
+        print(V2);
     }
 
     /* Alternativ lige som for, kunne bare ikke finde på noget*/
+    num = 0;
     while(num < 10)
     {
         if(num != 0)
