324a8ab9ce7e1708aa139db687f29952d8a1657b
mathiassmichno <mmichn13@student.aau.dk>
2015-05-13 10:49:37 +0200

    fix calloc

--- seppuku/resources/codesnippets/makeMatrix.c
+++ seppuku/resources/codesnippets/makeMatrix.c
@@ -1,3 +1,3 @@
 //MATRIX GENERATION FOR §ID§ {cols, rows, dataSize, dataStart}
-matrix §ID§ = {§COLS§, §ROWS§, sizeof(§SIMPLETYPE§) * §COLS§ * §ROWS§, calloc(§COLS§ * §ROWS§, sizeof(§SIMPLETYPE§) * §COLS§ * §ROWS§)};
+matrix §ID§ = {§COLS§, §ROWS§, sizeof(§SIMPLETYPE§) * §COLS§ * §ROWS§, calloc(§COLS§ * §ROWS§, sizeof(§SIMPLETYPE§))};
 //END MATRIX GENERATION
